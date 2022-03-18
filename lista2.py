5264df2857e5b6bad9648c9af70eeb4d8f87cd27
Cr3Van <74877467+Cr3Van@users.noreply.github.com>
2022-03-18 13:54:02 +0100

    Add files via upload

---
+++ lista2.py
@@ -0,0 +1,72 @@
+import math
+import sys as system
+#zadanie1
+# sporty = ['Piłka Nożna','Siatkówka','Tenis Stołowy']
+# sporty.reverse()
+# sporty.append('koszykówka')
+# sporty.append('f1')
+# print(sporty)
+#zadanie2
+# slownik = {'np': 'Na przykład',
+#            'etc': 'Et cetera',
+#            'red': 'redaktor'}
+# print (slownik)
+#zadanie3
+# gry = {'strategie': ['Europa Universalis IV','Hearts of Iron IV'],
+#        'moba': ['Dota 2','Smite','League of Legends'],
+#        'sportowe': ['f12021','fifa22','nba2k22']
+# }
+# print(gry)
+#zadanie4
+
+# zdanie=input()
+# print(zdanie.count('a'))
+
+#zadanie5
+# system.stdout.write("wpisz 3 liczby calkowite: ")
+# a = int(system.stdin.readline())
+# b = int(system.stdin.readline())
+# c = int(system.stdin.readline())
+# wynik = (pow(a,b)+c)
+# system.stdout.write(str(wynik))
+#zadanie6
+# a=input()
+# b=input()
+# c=input()
+#
+# max = a
+# if (a<b):
+#     max = b
+#     if(b<c):
+#         max = c
+# print("Najwieksza liczba to:",max)
+
+#zadanie7
+# lista=[3,3.5,6,8.4,10]
+# x=0
+# for a in lista:
+#     lista[x]=pow(a,2)
+#     x+=1
+# print(lista)
+#zadanie8
+# zmienna = []
+# parz = []
+# a = 0
+# b = 0
+# while a < 10:
+#     zmienna[a]=input()
+#     if (zmienna[a] % 2 == 0):
+#         parz[b]=zmienna[a]
+#         b+=1
+#     a+=1
+# print(parz)
+#zadanie9
+# print("Podaj swoja liczbę: ")
+# a=int(input())
+# if (a<0):
+#     print("Podales liczbę ujemna")
+# else:
+#     print("pierwiastek z twojej liczby to: ")
+#     print(math.sqrt(a))
+
+
