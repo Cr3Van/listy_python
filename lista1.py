c56ac86d6001712a4d7c66002910901e5880116e
Cr3Van <74877467+Cr3Van@users.noreply.github.com>
2022-03-18 11:05:46 +0100

    Add files via upload

---
+++ lista1.py
@@ -0,0 +1,61 @@
+import math
+# 1zadanie
+# a = 3
+# b = 7
+# c = 5.3
+# d = 7.4
+# e = 3+2j
+# f = 5+4j
+# imie = "Piotr"
+# nazwisko = "Zienkiewicz"
+# print (a,b)
+# print (c,d)
+# print (e,f)
+# print (imie,nazwisko)
+#2zadanie
+
+# a=8
+# b=6
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a/b)
+
+#3zadanie
+
+# a = 14
+# b = 3
+# print("Wybierz dzialanie +dodawanie,-odejmowanie,*-mnozenie,/-dzielenie")
+# dzialanie=input()
+# if(dzialanie == '+'):
+#     print(a+b)
+# elif(dzialanie == '-'):
+#     print(a-b)
+# elif(dzialanie == '*'):
+#     print(a*b)
+# elif(dzialanie == '/'):
+#     print(a/b)
+# else:
+#     print("uzyles nieprawidlowego znaku")
+
+#5zadanie
+# a = "PIOTR"
+# b = "ZIENKIEWICZ"
+# capA=a.capitalize()
+# capB=b.capitalize()
+# print(capA,capB)
+#6zadanie
+# piosenka = "Jestem piekny la la la"
+# print(piosenka.count("la"))
+#7zadanie
+# imie="Piotr"
+# print(imie[1],imie[4])
+#8zadanie
+# print(piosenka.split("pie"))
+#9zadanie
+# tekst="abcdefg"
+# b=3.4
+# c=hex(346)
+# print (tekst,b,c)
+
+
