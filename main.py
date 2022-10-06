dc9302119ab8ec4c56d322c621421f8d9f4aeaab
Piotr Zienkiewicz <74877467+Cr3Van@users.noreply.github.com>
2022-10-06 12:58:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,168 +1,54 @@
-import math
-# lista = []
-# 1a
-# lista = []
-# for i in range(1,11):
-#     lista.append(i)
-# print(lista)
+#1,2
+# def przedstaw(imie,nazwisko):
+#     print(imie[0].capitalize()+'.'+nazwisko.capitalize())
+# print("Podaj imie: ")
+# imie = input()
+# print("Podaj nazwisko: ")
+# nazwisko = input()
+# przedstaw(imie,nazwisko)
 
-# 1b
-# lista = []
-# for i in range(0,21):
-#     if(i%2==0):
-#         lista.append(i)
-# print
-
-# 1c
-# lista = []
-# for i in range(1,11):
-#     lista.append(math.pow(i,2))
-# print(lista)
-
-# 1d
-# lista = []
-# for i in range(1,11):
-#     lista.append(i*0)
-# print
-
-# 1e
-# lista = []
-# for i in range(1,11):
-#     if(i%2==0):
-#         lista.append(1)
+#3
+#nieskonczone
+# def ile_lat(a,b,wiek):
+#     if(wiek>b):
+#         a = a-1
+#         b = (b+100) - wiek
+#         rok_ur = str(a + b)
 #     else:
-#         lista.append(0)
-# print
+#         b = b-wiek
+#         rok_ur = str(a + b)
+#     return rok_ur
+#
+# a = input("dwie pierwsze cyfry roku: ")
+# b = input("dwie nastepne cyfry roku: ")
+# wiek = input("Podaj wiek: ")
+# print(ile_lat(a,b,wiek))
+#
 
-# 1f
+
+# 6
+# suma = 0
+# while (suma<100):
+#     a = int(input())
+#     suma = suma + a
+#     print("aktualna suma:",suma)
+
+
+
+#7
+# a=input("podaj ilosc elementow listy")
+# def kr(lista):
+#     kr = tuple(lista)
+#     return kr
 # lista = []
-# for i in range (1,11):
-#     if(i<6):
-#        lista.append(i-1)
-#     elif(i>6):
-#     lista.append(i-6)
-#     else:
-#         lista.append(0)
-#     print(lista)
-
-#2a
-# lista = []
-# i = 1
-# while i<11:
-#     lista.append(i)
-#     i += 1
-# print(lista)
-
-# 2b
-# lista = []
-# i = 1
-# while i<21:
-#     if(i%2==0):
-#         lista.append(i)
-#     i += 1
-# print(lista)
-
-# 2c
-# i=1
-# while i<11:
-#     lista.append(math.pow(i,2))
-#     i += 1
-# print(lista)
-
-# 2d
-# i=1
-# while i<11:
-#     lista.append(0)
-#     i += 1
-# print(lista)
-
-# 2e
-# i=1
-# while i<11:
-#     if(i%2==0):
-#         lista.append(1)
-#     else:
-#         lista.append(0)
-#     i+=1
-# print(lista)
+# while len(lista) < a:
+#     a = input()
+#     lista.append(a)
 #
 
-# 2f
-
-# i=1
-# while i<11:
-#     if(i<6):
-#         lista.append(i-1)
-#     elif(i>6):
-#         lista.append(i-6)
-#     else:
-#         lista.append(0)
-#     i += 1
-# print(lista)
-
-# 3a
-# lista = [-4, 7, -3, 8]
-# def ile_ujemnych(lista):
-#     suma_ujemnych = 0
-#     for i in lista:
-#         if i<0:
-#             suma_ujemnych += 1
-#     return suma_ujemnych
-#
-# def iloczyn(lista):
-#     liczba = 1
-#     for i in lista:
-#         liczba = liczba * i
-#     return liczba
-#
-# def minmax(lista):
-#     min=10
-#     max=0
-#     for i in lista:
-#         if(i<min):
-#             min = i
-#         elif(i>max):
-#             max = i
-#     lista2=[]
-#     lista2.append(min)
-#     lista2.append(max)
-#     return lista2
-#
-# def suma_przemienna(lista):
-#     suma=0
-#     pom=1
-#     for i in lista:
-#         if(pom%2==1):
-#             suma=suma+i
-#         else:
-#             suma=suma-i
-#         pom+=1
-#     return suma
-#
-#
-#
-#
-#
-# def main():
-#     lista = [-4, 7, -3, 8]
-#     print(ile_ujemnych(lista))
-# print(minmax(lista))
-# print(suma_przemienna(lista))
-# 7
-# lista=[]
-# def tw_listy(lista):
-#     for i in lista:
-#         a = input()
-#         if not(a == lista[1:10]):
-#             lista.append(a)
-#         if len(lista) == 10:
-#             break
-# tw_listy(lista)
-# print(lista)
-
-lista = []
-while len(lista) < 10:
-    a=input()
-    if not(a==lista[1:10]):
-        lista.append(a)
-print(lista)
+# 9
+# def dzien_tygodnia(a):
+#     kr = ('Niedziela','Poniedzialek','Wtorek','Sroda','Czwartek','Piatek','Sobota')
+#     return kr[a]
+# a = int(input('Podaj nr dnia tygodnia:'))
+# print(dzien_tygodnia(a))
